b4653a5cbf84eb380398ca207760c11173b0d501
zeroAska <trotwoodray@gmail.com>
2022-08-24 16:20:52 -0400

    add latex configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
+;;; package --- Sumnmary
+;;; Commentary:
+;;; Code:
+
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list
@@ -14,7 +18,7 @@
    (unless (package-installed-p package)
      (package-install package)))
 
-(dolist (package '(ivy counsel-gtags semantic flylisp flycheck-inline flycheck-irony compact-docstrings company-shell dtrt-indent smartparens yasnippet hl-todo auto-highlight-symbol multi-term elpy stickyfunc-enhance monokai-theme monokai-alt-theme helm-etags-plus function-args flycheck-clang-analyzer paren-face cuda-mode cpputils-cmake company-irony-c-headers company-irony company-c-headers common-lisp-snippets cmake-project cmake-mode auto-correct auto-complete-c-headers ac-slime ac-clang ac-c-headers yaml-mode hl-anything cdb julia-mode counsel-etags  ))
+(dolist (package '(ivy counsel-gtags semantic flylisp flycheck-inline flycheck-irony compact-docstrings company-shell dtrt-indent smartparens yasnippet hl-todo auto-highlight-symbol multi-term elpy stickyfunc-enhance monokai-theme monokai-alt-theme helm-etags-plus function-args flycheck-clang-analyzer paren-face cuda-mode cpputils-cmake company-irony-c-headers company-irony company-c-headers common-lisp-snippets cmake-project cmake-mode auto-correct auto-complete-c-headers ac-slime ac-clang ac-c-headers yaml-mode hl-anything cdb julia-mode counsel-etags  helm-gtags modern-cpp-font-lock lsp-mode auctex latex-preview-pane pdf-tools))
  (unless (package-installed-p package)
    (package-install package)))
 
@@ -27,16 +31,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(monokai))
  '(custom-safe-themes
-   '("d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "d1ede12c09296a84d007ef121cd72061c2c6722fcb02cb50a77d9eae4138a3ff" default))
+   '("78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" "d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "d1ede12c09296a84d007ef121cd72061c2c6722fcb02cb50a77d9eae4138a3ff" default))
+ '(helm-completion-style 'helm)
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(lice markdown-preview-mode markdown-mode counsel-etags company-rtags rtags company-jedi paren-face paredit paren-face scheme-complete docker julia-shell julia-mode julia-repl flycheck-julia haskell-mode ta ssh cdb rustic flycheck-rust cargo company counsel-gtags flylisp flycheck-inline flycheck-irony compact-docstrings company-shell dtrt-indent smartparens yasnippet hl-todo auto-highlight-symbol multi-term elpy stickyfunc-enhance monokai-theme monokai-alt-theme helm-etags-plus function-args flycheck-clang-analyzer cuda-mode cpputils-cmake company-irony-c-headers company-irony company-c-headers common-lisp-snippets cmake-project cmake-mode auto-correct auto-complete-c-headers ac-slime ac-clang ac-c-headers)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   '( pdf-tools lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode)))
 
 
 ;;(setq semantic-c-obey-conditional-section-parsing-flag nil)
@@ -57,12 +56,12 @@
 (electric-pair-mode 1)
 
 ;; for ctags
-(setq path-to-ctags "/usr/bin/ctags") 
-(defun create-tags (dir-name)
-    "Create tags file."
-    (interactive "DDirectory: ")
-    (shell-command
-     (format "%s -f TAGS -e -R %s" path-to-ctags (directory-file-name dir-name))))
+;(setq path-to-ctags "/usr/bin/ctags") 
+;(defun create-tags (dir-name)
+;    "Create tags file."
+;    (interactive "DDirectory: ")
+;    (shell-command
+;     (format "%s -f TAGS -e -R %s" path-to-ctags (directory-file-name dir-name))))
 
 
 (setq-default indent-tabs-mode nil)
@@ -77,7 +76,7 @@
 (column-number-mode 1)
 
 ;; auto complete
-(global-auto-complete-mode t)
+;(global-auto-complete-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; for c++, cuda
@@ -85,67 +84,53 @@
 (require 'cc-mode)
 ;(add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
+;; for cuda 
+;;(add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-mode))
+;;(add-to-list 'auto-mode-alist '("\\.cuh\\'" . c++-mode))
+
 ; (fa-config-default)
 ;(add-hook 'c-mode-common-hook
 ;          (lambda ()
 ;            (if (derived-mode-p 'c-mode 'c++-mode)
 ;                (cppcm-reload-all)
-;              )))
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (if (derived-mode-p 'c-mode 'c++-mode)
-                (if  (not (or (string-match "^/usr/local/include/.*" buffer-file-name)
-                              (string-match "^/usr/src/linux/include/.*" buffer-file-name)))
-                    (cppcm-reload-all))
-              )))
-(add-hook 'c-mode-hook 'irony-mode)
-(defun my-irony-mode-hook ()
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-;;(eval-after-load 'company
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-(require 'company-c-headers)
-(require 'company-irony-c-headers)
-(eval-after-load 'company
-  '(add-to-list
-    'company-backends '(company-irony-c-headers company-irony)))
-(setq company-idle-delay nil)
-(setq company-minimum-prefix-length 1)
-; Use tab key to cycle through suggestions.
-; ('tng' means 'tab and go')
-(company-tng-configure-default)
-;; weight by frequency
-(setq company-transformers '(company-sort-by-occurrence))
-(setq company-backends (delete 'company-semantic company-backends))
-;(setq company-backends (delete 'company-dabbrev company-backends))
-;(global-set-key "\t" 'company-complete-common)
-;;(define-key c-mode-map [(tab)] 'company-complete)
-;(define-key c++-mode-map [(tab)] 'company-complete)
-;(add-hook 'c++-mode-hook 'flycheck-mode)
-;(add-hook 'c-mode-hook 'flycheck-mode)
-;(add-to-list 'company-irony-c-headers-path-system "/usr/include/c++/5/")
-(add-hook 'c++-mode-hook '(lambda () (which-func-mode t)))
-(add-hook 'c++-mode-hook '(lambda () (linum-mode t)))
-(with-eval-after-load 'company
-  (company-ctags-auto-setup))
-(setq company-ctags-extra-tags-files '("$HOME/TAGS" "/usr/include/TAGS" "/usr/local/include/TAGS"))
-;;  '(add-to-list 'company-backends 'company-irony))
+                                        ;              )))
 
 
-;; for lisp environment
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
-;(setq slime-contribs '(slime-fancy))
 
-;; syntax check
-(global-flycheck-mode)
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp
+    projectile hydra flycheck company avy which-key helm-xref dap-mode))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
+(helm-mode)
+(require 'helm-xref)
+(define-key global-map [remap find-file] #'helm-find-files)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(which-key-mode)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+(add-hook 'python-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
+
 
 ;; highlight current light
 (global-hl-line-mode +1)
@@ -154,6 +139,15 @@
 ;; for shell color
  (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
+
+(require 'flycheck)
+;; Force flycheck to always use c++11 support. We use
+;; the clang language backend so this is set to clang
+(add-hook 'c++-mode-hook
+          (lambda () (setq flycheck-clang-language-standard "c++11")))
+;(eval-after-load 'flycheck
+;  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+(global-flycheck-mode)
 
 
 ;; for yaml
@@ -164,33 +158,8 @@
 (add-hook 'after-init-hook 'global-auto-highlight-symbol-mode)
 
 ;; enable search at current symbol
-(global-set-key (kbd "C-S-s") 'isearch-forward-symbol-at-point)
+(global-set-key (kbd "M-s") 'isearch-forward-symbol-at-point)
 
-
-;; sticky function mode -> show which function you are in currently
-;(global-semantic-idle-scheduler-mode 1)
-;(semantic-enable)
-(semantic-mode 1)
-(add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
-(require 'stickyfunc-enhance)
-(which-function-mode 1)
-
-
-;; auto complete templates
-(require 'yasnippet)
-(yas-global-mode 1)
-(defun yas-no-expand-in-comment/string ()
-  (setq yas-buffer-local-condition
-        '(if (nth 8 (syntax-ppss)) ;; non-nil if in a string or comment
-             '(require-snippet-condition . force-in-comment)
-           t)))
-(add-hook 'c-mode-hook 'yas-no-expand-in-comment/string)
-(add-hook 'c++-mode-hook 'yas-no-expand-in-comment/string)
-
-
-;; Package: smartparens
-;; when you press RET, the curly braces automatically
-;; add another newline
 (require 'smartparens-config)
 (show-smartparens-global-mode +1)
 ;;(smartparens-global-mode 1)
@@ -203,27 +172,6 @@
 (require 'dtrt-indent)
 (dtrt-indent-mode 1)
 (setq dtrt-indent-verbosity 0)
-
-
-
-
-;; semantic completion
-;(require 'semantic)
-;(global-semanticdb-minor-mode 1)
-;(global-semantic-idle-scheduler-mode 1)
-;(semantic-mode 1)
-;(semantic :disabled-for emacs-lisp)
-
-	;; clear eshell
-;(defun eshell/clear ()
- ; "Clear the eshell buffer."
-  ;(let ((inhibit-read-only t))
-   ; (erase-buffer)
-    ;    (eshell-send-input)))
-
-;; for cuda 
-(add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.cuh\\'" . c++-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; for julia
@@ -282,30 +230,52 @@
 (require 'paren-face)
 (global-paren-face-mode 1)
 (show-paren-mode 1)
-;;(set-face-foreground 'paren-face "DimGray")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 
-;; for ctags
-(setq path-to-ctags "/usr/bin/ctags") 
-(defun create-tags (dir-name)
-  "Create tags file."
-  (interactive "DDirectory: ")
-  (shell-command
-   (format "find %s -name '*.hpp' -o -name '*.h' -o -name '*.cuh' -o -name '*.py' -o -name '*.c' -o -name '*.cpp' -o -name '*.cu' | xargs %s -f TAGS -e -R" (directory-file-name dir-name) path-to-ctags))
+
+;; for eshall color
+(add-hook 'eshell-preoutput-filter-functions
+          'ansi-color-filter-apply)
+
+;; for python
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt --InteractiveShell.display_page=True")
+
+
+;; for latex
+(require 'gscholar-bibtex)
+(use-package tex-site
+  :defer t
+  :ensure auctex
+  :mode ("\\.tex\\'" . latex-mode)
+  :config (progn
+            (setq TeX-auto-save t)
+            (setq TeX-parse-self t)
+            (setq-default TeX-master nil)
+            (setq TeX-command-force "LaTeX")
+            ;(setq TeX-clean-confirm t)
+            (setq TeX-view-program-selection
+                  '((output-dvi "PDF Tools")
+                    (output-pdf "PDF Tools")))
+            (add-hook 'LaTeX-mode-hook 'visual-line-mode)
+            (add-hook 'LaTeX-mode-hook '(lambda () Tex-source-correlate-mode))
+            (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)       
+            ;(add-hook 'LaTeX-mode-hook 'latex-preview-pane-mode)
+            (add-hook 'LaTeX-mode-hook 'linum-mode)
+            (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+            (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+            (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+            (add-hook 'pdf-view-mode-hook
+                      (lambda ()
+                        (pdf-view-fit-page-to-window) ))
+                                       ;(setq reftex-plug-into-AUCTeX t)
+            (setq TeX-PDF-mode t)
+            (latex-preview-pane-enable)
   )
-(define-key global-map "\M-*" 'pop-tag-mark)
-
-
-
-;; for custom files to load
-;; for auto header insert
-(add-to-list 'load-path "~/.emacs.d/custom")
-(require 'header2)
-;(autoload 'auto-update-file-header "header2")
-;(add-hook 'write-file-hooks 'auto-update-file-header)
-;(autoload 'auto-make-header "header2")
-;(add-hook 'emacs-lisp-mode-hook 'auto-make-header)
-;(add-hook 'c-mode-common-hook   'auto-make-header)
-;(add-hook 'c++-mode-common-hook   'auto-make-header)
-;(add-hook 'python-mode-common-hook   'auto-make-header)
-
+)
